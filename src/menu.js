1b86950f64786708297f3834e03528c0cf8aa07d
shubhangini tripathy <shubhanginitripathy@gmail.com>
2018-10-13 18:16:33 +0530

    Add split summary page

--- src/menu.js
+++ src/menu.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { Label, List, Image, Grid } from 'semantic-ui-react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import menuList from './menu.js'
 
 function getStatus(amount) {
   if (amount === 0) {
     return "settled up";
   } else if (amount > 0) {
-    return "owes you";
+    return "you lent";
   } else {
-    return "you owe";
+    return "you borrowed";
   }
 };
 
-function getStatusClass(amount) {
+function getMenuClass(amount) {
   const badge = "badge ";
   if (amount === 0) {
     return badge + "badge-secondary";
@@ -23,18 +22,14 @@ function getStatusClass(amount) {
     return badge + "badge-danger";
   }
 };
-function myFunction() {
-  alert("I am an alert box!");
-}
-export default function splitApp() {
 
+export default function menuList() {
   var statusByFriends = [
-    { name: 'Shubhi', amount: '0' },
-    { name: 'Ashu', amount: '1' },
-    { name: 'Roli', amount: '-1' },
-    { name: 'Jhinu', amount: '1' }
+    { name: 'Lunch', amount: '0' },
+    { name: 'dinner', amount: '1' },
+    { name: 'Breakfast', amount: '-1' },
+    { name: 'Night Out', amount: '1' }
   ];
-
 
   return (
     <div style={{ margin: '10px' }}>
@@ -43,16 +38,13 @@ export default function splitApp() {
           statusByFriends.map((item, index) => (
             <li class="list-group-item d-flex justify-content-between align-items-center">
               {item.name}
-              <span onClick={myFunction} class={getStatusClass(item.amount)}>{getStatus(item.amount) + ": " + item.amount}</span>
+              <span class={getMenuClass(item.amount)}>{getStatus(item.amount) + ": " + item.amount}</span>
 
             </li>
           ))
         }
       </List>
-      {menuList()}
     </div>
-
-
   );
 };
 
